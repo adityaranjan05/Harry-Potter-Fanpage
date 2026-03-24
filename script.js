6565866a70415efe8c1e0e3b13379ab5cfa3478b
Aditya Ranjan Sinha <aditya.ranjan.cs@gmail.com>
2026-03-24 23:19:21 +0530

    Added characters page

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
 "use strict";
 
-// Navbar sections
-
-const links = document.querySelectorAll("#navbar .links a");
-const pages = document.querySelectorAll(".page");
-
-links.forEach(link => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const pageId = link.dataset.page;
-
-    pages.forEach(page => page.classList.remove("active"));
-    document.getElementById(pageId).classList.add("active");
-  });
-});
-
