e01ae2e7ab27caabfd9c5f492800adf77b828882
Aditya Ranjan Sinha <aditya.ranjan.cs@gmail.com>
2026-03-24 21:59:51 +0530

    Did some changes

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const links = document.querySelectorAll("#navbar .links a");
+const pages = document.querySelectorAll(".page");
+
+links.forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const pageId = link.dataset.page;
+
+    pages.forEach(page => page.classList.remove("active"));
+    document.getElementById(pageId).classList.add("active");
+  });
+});
