a1c479a870d2af3035c6d39c265035e37d5693ba
Aditya Ranjan Sinha <aditya.ranjan.cs@gmail.com>
2026-03-25 18:38:56 +0530

    Added Characters Card

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 "use strict";
 
+// Characters Page
+
+let ch_url = "https://potterapi-fedeperin.vercel.app/en/characters";
+let cards = document.querySelectorAll(".cards");
+
+fetch(ch_url)
+  .then((result) => result.json())
+  .then((data) => {
+    console.log(data);
+
+    cards.forEach((card, index) => {
+      if (data[index]) {
+        card.querySelector(".image").src = data[index].image || "";
+        card.querySelector(".fname").textContent = "Full Name: " + data[index].fullName || "Unknown";
+        card.querySelector(".house").textContent = "Hogwarts House: " + data[index].hogwartsHouse || "Unknown";
+        card.querySelector(".birthday").textContent = "Birthdate: " + data[index].birthdate || "Unknown";
+      }
+    });
+  })
+  .catch((error) => console.log(error));
